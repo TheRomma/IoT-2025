48262df2cb968e51e454f1c8a6bbea0e9e21f704
treinikk20 <treinikk20@student.oulu.fi>
2025-11-23 16:54:05 +0200

    Add sending of measurement data to mqtt broker

---
+++ sensor/mqtt_sender.py
@@ -0,0 +1,52 @@
+import ujson
+from umqtt.simple import MQTTClient
+
+class MQTTSender:
+    def __init__(self, server, user=None, password=None,
+                 topic="sensors/measurements", client_id="pico_client"):
+
+        self.topic = topic
+
+        self.client = MQTTClient(
+            client_id=client_id,
+            server=server,
+            user=user,
+            password=password,
+            port=1883,
+            keepalive=60
+        )
+
+        self.connected = False
+
+    def connect(self):
+        """Connect to the MQTT broker."""
+        try:
+            self.client.connect()
+            self.connected = True
+            print("MQTT connected!")
+        except Exception as e:
+            print("MQTT connection failed:", e)
+
+    def publish(self, sensor_id, location, temperature, pressure):
+        """Publish a JSON message."""
+        if not self.connected:
+            self.connect()
+
+        payload = ujson.dumps({
+            "sensor_id": sensor_id,
+            "location": location,
+            "temperature": round(temperature, 2),
+            "pressure": round(pressure, 2)
+        })
+
+        try:
+            self.client.publish(self.topic, payload)
+            print("MQTT published:", payload)
+        except Exception as e:
+            print("MQTT publish failed:", e)
+
+    def disconnect(self):
+        try:
+            self.client.disconnect()
+        except:
+            pass
--- sensor/sensor_main.py
+++ sensor/sensor_main.py
@@ -1,12 +1,26 @@
 from wifi import connect_wifi
 from delay_server import DelayServer
-from bmp import BMP280Reader
+from sensor_reader import BMP280Reader
+from mqtt_sender import MQTTSender
 
 from machine import Pin, I2C
 from time import ticks_ms, ticks_diff
 
-SSID = "maksamakkara"
-PASS = "joulupukki123"
+SSID = ""
+PASS = ""
+
+MQTT_SERVER = "127.0.0.1"
+MQTT_USER = "mqttuser"
+MQTT_PASS = "mqttpass"
+MQTT_TOPIC = "sensors/measurements"
+
+mqtt = MQTTSender(
+    server=MQTT_SERVER,
+    user=MQTT_USER,
+    password=MQTT_PASS,
+    topic=MQTT_TOPIC,
+    client_id="pico_0"
+)
 
 wlan = connect_wifi(SSID, PASS)
 print("Device IP:", wlan.ifconfig()[0])
@@ -19,6 +33,8 @@ server.start()
 print("Web UI ready at: http://{}".format(wlan.ifconfig()[0]))
 
 last = ticks_ms()
+
+mqtt.connect()
 
 while True:
     server.poll()
@@ -33,5 +49,11 @@ while True:
         print(
             "Temp: {:.2f} C  Pressure: {:.2f} hPa   Delay: {}".format(
                 t, p, server.get_delay()
-            )
+            ))
+
+        mqtt.publish(
+            sensor_id="sensor_pico_0",
+            location="olohuone",
+            temperature=t,
+            pressure=p
         )
